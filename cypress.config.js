98a2737477f74c67de34b6decaea48d66b401e5e
Francisco Bandeira <francisco.frazzon@outlook.com>
2024-04-12 15:42:44 -0300

    correção ci

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
+  video: true,
+  videoCompression: true,
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+     
     },
   },
 });
